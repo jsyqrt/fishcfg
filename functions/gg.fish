c5c618a3e4ab1d53bfd5580769e83e1316ac1527
Jason Carter <jsyqrt@gmail.com>
2018-09-16 12:40:07 +0800

    nothing

--- functions/gg.fish
+++ functions/gg.fish
@@ -7,28 +7,36 @@ function gga
     git add $argv
 end
 
+function ggc
+    git commit -m $argv
+end
+
 function ggf
     git fetch
 end
 
+function ggg
+    git grep $argv
+end
+
 function ggh
     echo "GG Usage:"
-    echo "      gg      git status"
-    echo "      gga     git add"
-    echo "      ggf     git fetch"
-    echo "      ggh     print this help"
-    echo "      ggl     git log"
-    echo "      ggm     git commit"
-    echo "      ggpl    git pull"
-    echo "      ggps    git push"
+    echo "      gg          git status"
+    echo "      gga         git add "
+    echo "      ggc         git commit -m $argv"
+    echo "      ggf         git fetch"
+    echo "      ggg         git grep $argv"
+    echo "      ggh         print this help"
+    echo "      ggl         git log"
+    echo "      ggpl        git pull"
+    echo "      ggps        git push $argv"
+    echo "      ggrb        git rebase -i master"
+    echo "      ggrba       git rebase --abort"
+    echo "      ggrbc       git rebase --continue"
 end
 
 function ggl
     git log
-end
-
-function ggm
-    git commit -m $argv
 end
 
 function ggpl
@@ -38,3 +46,17 @@ end
 function ggps
     git push $argv
 end
+
+function ggrb
+    git rebase -i master
+end
+
+function ggrba
+    git rebase --abort
+end
+
+function ggrbc
+    git rebase --continue
+end
+
+
