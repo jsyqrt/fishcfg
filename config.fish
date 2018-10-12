293264f78eb78bd2bd88cd50b122d862f8318ba7
Liu Qian <liuqian@cpchain.io>
2018-10-12 18:53:35 +0800

    nothing.

--- config.fish
+++ config.fish
@@ -1,11 +1,11 @@
-abbr --add gg git status
+abbr --add gg git status 
 abbr --add gga git add 
 abbr --add ggb git branch 
-abbr --add ggbd git branch -d
-abbr --add ggbdd git branch -D
+abbr --add ggbd git branch -d 
+abbr --add ggbdd git branch -D 
 abbr --add ggc git commit -m  
 abbr --add ggch git checkout 
-abbr --add ggchb git add checkout -b
+abbr --add ggchb git add checkout -b 
 abbr --add ggd git diff 
 abbr --add ggdm git diff --name-only master
 abbr --add ggdn git diff --name-only 
@@ -13,9 +13,10 @@ abbr --add ggf git fetch
 abbr --add ggg git grep 
 abbr --add ggl git log 
 abbr --add ggm git merge 
+abbr --add ggm git merge master 
 abbr --add ggpl git pull 
 abbr --add ggps git push 
 abbr --add ggrb git rebase -i master 
-abbr --add ggrba git rebase --abort
-abbr --add ggrbc git rebase --continue
+abbr --add ggrba git rebase --abort 
+abbr --add ggrbc git rebase --continue 
 
