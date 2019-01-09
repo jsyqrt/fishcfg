074165868970d5c8f8197ad807b40b64c2140c3a
Liu Qian <liuqian@cpchain.io>
2019-01-09 18:56:07 +0800

    minor

--- config.fish
+++ config.fish
@@ -1,31 +1,33 @@
-abbr --add gg git status 
-abbr --add gga git add 
-abbr --add ggb git branch 
-abbr --add ggbd git branch -d 
-abbr --add ggbdd git branch -D 
-abbr --add ggc git commit -m  
-abbr --add ggch git checkout 
-abbr --add ggchb git checkout -b
-abbr --add ggchm git checkout master
-abbr --add ggd git diff 
-abbr --add ggdm git diff master
-abbr --add ggdmn git diff --name-only master
-abbr --add ggdn git diff --name-only 
-abbr --add ggf git fetch 
-abbr --add ggg git grep 
-abbr --add ggl git log 
-abbr --add ggm git merge 
-abbr --add ggma git merge --abort
-abbr --add ggmc git merge --continue
-abbr --add ggmm git merge master 
-abbr --add ggpl git pull 
-abbr --add ggps git push 
-abbr --add ggrb git rebase -i master 
-abbr --add ggrba git rebase --abort 
-abbr --add ggrbc git rebase --continue 
-abbr --add ggs git stash
-abbr --add ggsl git stash list
-abbr --add ggsp git stash pop
+abbr --add g git status
+abbr --add ga git add
+abbr --add gb git branch
+abbr --add gbd git branch -d
+abbr --add gbdd git branch -D
+abbr --add gc git commit -m
+abbr --add gch git checkout
+abbr --add gchb git checkout -b
+abbr --add gchm git checkout master
+abbr --add gd git diff
+abbr --add gdm git diff master
+abbr --add gdmn git diff --name-only master
+abbr --add gdn git diff --name-only
+abbr --add gf git fetch
+abbr --add gg git grep
+abbr --add gl git log
+abbr --add gm git merge
+abbr --add gma git merge --abort
+abbr --add gmc git merge --continue
+abbr --add gmm git merge master
+abbr --add gpl git pull
+abbr --add gps git push
+abbr --add grb git rebase -i master
+abbr --add grba git rebase --abort
+abbr --add grbc git rebase --continue
+abbr --add gs git stash
+abbr --add gsl git stash list
+abbr --add gsp git stash pop
 
+abbr --add l clear;ls
+abbr --add c cd
 
 alias e emacs
